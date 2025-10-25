de932be928b5e0e8160f87542e6147c31d25e8fa
Syaiful Nur Wardani <79695832+SyaifulNW@users.noreply.github.com>
2025-10-25 14:13:09 +0700

    mantap

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ AOS.init({
 });
 
 
+  const menuToggle = document.querySelector("#mobile-menu");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("active");
+    navLinks.classList.toggle("active");
+  });
